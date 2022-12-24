6d7cc638767fa93d59a22fb1d087cfdd1309fc7a
triskj0 <108220065+triskj0@users.noreply.github.com>
2022-12-24 20:03:02 +0100

    added graphs + screenshots

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # one-layer-perceptron
+- a very **simple** implementation of the perceptron learning algorithm
+- **graphs** everything using matplotlib.pyplot
+- very **detailed comments**
+
+![cmd_screenshot](https://github.com/triskj0/one-layer-perceptron/blob/main/screenshots/cmd_screenshot.png)
+
+![graph_screenshot](https://github.com/triskj0/one-layer-perceptron/blob/main/screenshots/graph_screenshot.png)
--- perceptron.py
+++ perceptron.py
@@ -1,24 +1,27 @@
 import math
 import random
+# libraries used for the graph:
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 LEARNING_RATE = .1
 TOTAL_ITERATIONS = 100
 
-x_values = [] # x coordinates
-y_values = [] # y coordinates
-outputs = [] # the correct answers
+patterns = []
 
 # read data from a file
-with open('test_data.txt', 'r') as f:
+with open('test1.txt', 'r') as f:
 	while 1:
 		line = f.readline().strip().split()
 		if not line:
 			break
-		x_values.append(float(line[0]))
-		y_values.append(float(line[1]))
-		outputs.append(float(line[2]))
-
+		patterns.append([float(line[0]), float(line[1]), float(line[2])])
+'''
+patterns[n][0] is the x coordinate
+patterns[n][1] is the y coordinate
+patterns[n][2] is the correct output (1 or -1)
+'''
 
 def calculate_output(weights, bias, x, y):
 	output = x * weights[0] + y * weights[1] + bias
@@ -39,6 +42,17 @@ def update_params(weights, bias, err, x, y):
 	return weights, bias
 
 
+def graph_points():
+	# assuming that there will always be a list of patterns called 'patterns'
+	for pattern in patterns:
+		plt.plot(pattern[0], pattern[1], 'ro' if pattern[2] == 1 else 'go')
+
+
+def graph_line(x_multiplicand, y_multiplicand, bias):
+	x = np.linspace(-10, 10, 100)
+	plt.plot(x, (x_multiplicand*x + bias)/-y_multiplicand, '-b')
+
+
 def gradient_descent():
 	# initialize
 	weights, bias = init_params()
@@ -47,7 +61,7 @@ def gradient_descent():
 	for iteration in range(TOTAL_ITERATIONS):
 		global_err = 0
 		# iterate over each pattern
-		for pattern in zip(x_values, y_values, outputs):
+		for pattern in patterns:
 			# calculate output and local error
 			output = calculate_output(weights, bias, pattern[0], pattern[1])
 			local_err = pattern[2] - output
@@ -56,16 +70,31 @@ def gradient_descent():
 			global_err += local_err**2
 
 		# RMSE = root mean squared error
-		rmse = '{:.3f}'.format(math.sqrt(global_err/len(x_values)))
+		rmse = '{:.3f}'.format(math.sqrt(global_err/len(patterns)))
 		print(f'Iteration: {iteration+1} : RMSE = {rmse}')
 
-		# break if we predicted everything correctly
+		# break if everything's been predicted correctly
 		if global_err == 0:
 			break
 
-	# print the first four digits of each final value
-	print(f'\nDecision boundary line equation: {str(weights[0])[:4]}x + {str(weights[1])[:4]}y + {str(bias)[:4]} = 0')
+	'''
+	we need to transform the line values to this format: y = mx + b
+	0 = x_multiplicand*x + y_multiplicand*y + bias
+	=>	y = (x_multiplicand*x + bias)/-y_multiplicand
+	'''
+	# print the first three digits of each final value
+	line_equation = f'y = ({str(weights[0])[:4]}x + {str(bias)[:4]}) / -({str(weights[1])[:4]})'
+	print(f'\nDecision boundary line equation: {line_equation}')
+
+	return weights, bias
+
+
+def main():
+	weights, bias = gradient_descent()
+	graph_points()
+	graph_line(weights[0], weights[1], bias)
+	plt.show()
 
 
 if __name__ == '__main__':
-	gradient_descent()
+	main()
